ab1687f122020ac5c374471e74f64a4afcdbb4e5
Samuel Kleger <143487615+S-a-m-K@users.noreply.github.com>
2024-08-19 13:24:06 +0200

    Update 02. Data Exploration.sql

--- 02. Data Exploration.sql
+++ 02. Data Exploration.sql
@@ -1,1 +1,19 @@
+-- checking datatypes of all columns
+
+SELECT 
+  column_name,
+  data_type
+FROM 
+  `cyclistic-case-study-00.cyclistic_trip_data.INFORMATION_SCHEMA.COLUMNS`
+WHERE 
+  table_name = 'cyclistic_combined_trip_data_01'
+ORDER BY 
+  ordinal_position;
+
+
+-- count numbers of rows
+
+SELECT COUNT(*)
+FROM `cyclistic-case-study-00.cyclistic_trip_data.cyclistic_combined_trip_data_01`;
+
 
